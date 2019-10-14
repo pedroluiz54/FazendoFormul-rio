b4e202aaa2323a928d55e4848d9e1c7f6362f15f
AST <srmoura@outlook.com>
2019-10-14 14:37:19 -0300

    new branch test form

--- index.php
+++ index.php
@@ -5,48 +5,46 @@
 
 </head>
 <body>
-  <style>
-  body{background-color: 90EE90;}
- font-family: verdana
- </style>
+
+
 
 CADASTRO
 <form action="/script.php" method="post">
     <div>
-        <p><label for="nome">Nome:</label>
-        <p><input type="text" id="name" name="name"/>
+        <p><label for="nome">Nome:</label></p>
+        <p><input type="text" id="name" name="name"/></p>
     </div>
     <div>
-        <p><label for="cpf">CPF:</label>
-        <p><input type="text" id="cpf" name="cpf" />
+        <p><label for="cpf">CPF:</label></p>
+        <p><input type="text" id="cpf" name="cpf" /></p>
     </div>
     <div>
-        <p><label for="data">Data de nascimento:</label>
-        <p><input type="text" id="date" name="date"/>
+        <p><label for="data">Data de nascimento:</label></p>
+        <p><input type="text" id="date" name="date"/></p>
     </div>
     <div>
-        <p><label for="phone">Telefone:</label>
-        <p><input type="phone" id="mobile-number" name="mobile-number"/>
+        <p><label for="phone">Telefone:</label></p>
+        <p><input type="phone" id="mobile-number" name="mobile-number"/></p>
     </div>
     <div>
-        <p><label for="email">E-mail:</label>
-        <p><input type="email" id="email" name="email" />
+        <p><label for="email">E-mail:</label></p>
+        <p><input type="email" id="email" name="email" /></p>
     </div>
     <div>
-        <p><label for="cep">CEP:</label>
-        <p><input type="text" id="cep" name="cep"/>
+        <p><label for="cep">CEP:</label></p>
+        <p><input type="text" id="cep" name="cep"/></p>
     </div>
     <div>
-        <p><label for="endereço">Endereço:</label>
-        <p><input type="text" id="adress" name="adress"/>
+        <p><label for="endereço">Endereço:</label></p>
+        <p><input type="text" id="adress" name="adress"/></p>
     </div>
     <div>
-        <p><label for="número">Número:</label>
-        <p><input type="text" id="number" name="number"/>
+        <p><label for="número">Número:</label></p>
+        <p><input type="text" id="number" name="number"/></p>
     </div>
     <div>
-        <p><label for="complemento">Complemento:</label>
-        <p><input type="text" id="complement" name="complement"/>
+        <p><label for="complemento">Complemento:</label></p>
+        <p><input type="text" id="complement" name="complement"/></p>
     </div>
     <div class="button">
         <button type="submit">Enviar</button>
